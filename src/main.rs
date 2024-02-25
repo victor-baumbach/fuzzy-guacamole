b930c14fd695308f9f5f16b0e7ebf2d63afc9186
Victor Baumbach <8178548+victor-baumbach@users.noreply.github.com>
2024-02-25 17:12:12 +0000

    Column + Row Aligning example, amended author

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use druid::{AppLauncher, WindowDesc, Widget, PlatformError};
-use druid::widget::Label;
+use druid::widget::{Label, Flex, Padding, Align};
 
 fn build_ui() -> impl Widget<()> {
-    Label::new("Hello World")
+    Padding::new(
+        10.0,
+        Flex::row()
+            .with_flex_child(
+                Flex::column()
+                    .with_flex_child(Label::new("Top left"), 1.0)
+                    .with_flex_child(Align::centered(Label::new("Bottom left")), 1.0),
+            1.0)
+            .with_flex_child(
+                Flex::column()
+                    .with_flex_child(Label::new("Top right"), 1.0)
+                    .with_flex_child(Align::centered(Label::new("Bottom right")), 1.0),
+            1.0))
 }
 
 fn main() -> Result<(), PlatformError> {
